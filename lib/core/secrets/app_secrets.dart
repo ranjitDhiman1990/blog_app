77b2b8e9a1c2e40264f9068dc0f1b8ad8cddce4a
Dhiman Ranjit <dhimanranjit1990@gmail.com>
2024-10-04 00:58:15 +0530

    Secrets addes to .gitignore

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -3,3 +3,4 @@ class AppSecrets {
   static const supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkeGtyeGJ5aWVlcWh4YW92c3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5Njg4ODQsImV4cCI6MjA0MzU0NDg4NH0.sELJfYJQpTl3uxbl_aBm4M6FHG4VTbYLSgU9ElUNWbE';
 }
+//Supabase Password -> SupaBase#blog_app_2024
