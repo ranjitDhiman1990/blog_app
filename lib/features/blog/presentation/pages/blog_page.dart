6ca275b1c164fa506f020d77856e6d2f4440d783
Dhiman Ranjit <dhimanranjit1990@gmail.com>
2025-01-28 23:54:18 +0530

    Add New Blog static UI done

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BlogPage extends StatelessWidget {
@@ -8,6 +10,18 @@ class BlogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Blog Page"));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blog App'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, AddNewBlogPage.route());
+            },
+            icon: const Icon(CupertinoIcons.add_circled),
+          ),
+        ],
+      ),
+    );
   }
 }
